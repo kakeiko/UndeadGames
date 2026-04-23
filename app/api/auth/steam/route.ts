9bebb89cc0ffb588e76482f6f51e509c0c258aec
kakeiko <guidapm246@gmail.com>
2026-04-23 17:42:58 -0300

    v1.4

--- app/api/auth/steam/route.ts
+++ app/api/auth/steam/route.ts
@@ -4,8 +4,8 @@ export async function GET() {
   const params = new URLSearchParams({
     "openid.ns": "http://specs.openid.net/auth/2.0",
     "openid.mode": "checkid_setup",
-    "openid.return_to": `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/steam/return`,
-    "openid.realm": process.env.NEXT_PUBLIC_BASE_URL!,
+    "openid.return_to": `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/steam/return`,
+    "openid.realm": process.env.NEXT_PUBLIC_APP_URL!,
     "openid.identity": "http://specs.openid.net/auth/2.0/identifier_select",
     "openid.claimed_id": "http://specs.openid.net/auth/2.0/identifier_select",
   });
