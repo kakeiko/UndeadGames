df263609432a04be5b3fa3a672066d04da1ce3df
kakeiko <guidapm246@gmail.com>
2026-05-07 18:07:05 -0300

    v2.0

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://undeadgames.vercel.app" },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-        ],
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+}
 
 export default nextConfig;
 
